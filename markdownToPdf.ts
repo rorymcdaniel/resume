334e0091108b9d345be3286657b763be7ebb8852
Rory McDaniel <hoyer801@gmail.com>
2025-05-26 13:12:38 -0400

    fix: puppeteer executable

--- markdownToPdf.ts
+++ markdownToPdf.ts
@@ -1,27 +1,31 @@
 import { mdToPdf } from 'md-to-pdf';
 import * as path from 'path';
+import * as fs from 'fs';
 
 (async () => {
   const input = path.join(__dirname, 'README.md');
   const output = path.join(__dirname, 'output', 'resume.pdf');
 
+  process.env.PUPPETEER_EXECUTABLE_PATH = require('puppeteer').executablePath();
+
   await mdToPdf(
     { path: input },
     {
       css: path.join(__dirname, 'resumeStyle.css'),
-      launchOptions: {
-        args: ['--no-sandbox'],
-      }
+      pdf_options: {
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      } as any,
     } as any
-  ).then((pdf) => {
-    if (pdf?.content) {
-      const fs = require('fs');
-      fs.mkdirSync(path.dirname(output), { recursive: true });
-      fs.writeFileSync(output, pdf.content);
-      console.log('PDF generated successfully.');
-    }
-  }).catch((err) => {
-    console.error('Error generating PDF:', err);
-    process.exit(1);
-  });
+  )
+    .then((pdf) => {
+      if (pdf?.content) {
+        fs.mkdirSync(path.dirname(output), { recursive: true });
+        fs.writeFileSync(output, pdf.content);
+        console.log('PDF generated successfully.');
+      }
+    })
+    .catch((err) => {
+      console.error('Error generating PDF:', err);
+      process.exit(1);
+    });
 })();
