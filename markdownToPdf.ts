709aca75ffeac4fbe0e9242e5e928671c06ec4f7
Rory McDaniel <hoyer801@gmail.com>
2025-05-26 13:09:11 -0400

    fix: sandbox mode

--- markdownToPdf.ts
+++ markdownToPdf.ts
@@ -5,10 +5,23 @@ import * as path from 'path';
   const input = path.join(__dirname, 'README.md');
   const output = path.join(__dirname, 'output', 'resume.pdf');
 
-  await mdToPdf({ path: input }, { dest: output, css: path.join(__dirname, 'resumeStyle.css') })
-    .then(() => console.log('PDF generated successfully.'))
-    .catch((err) => {
-      console.error('Error generating PDF:', err);
-      process.exit(1);
-    });
+  await mdToPdf(
+    { path: input },
+    {
+      css: path.join(__dirname, 'resumeStyle.css'),
+      launchOptions: {
+        args: ['--no-sandbox'],
+      }
+    } as any
+  ).then((pdf) => {
+    if (pdf?.content) {
+      const fs = require('fs');
+      fs.mkdirSync(path.dirname(output), { recursive: true });
+      fs.writeFileSync(output, pdf.content);
+      console.log('PDF generated successfully.');
+    }
+  }).catch((err) => {
+    console.error('Error generating PDF:', err);
+    process.exit(1);
+  });
 })();
